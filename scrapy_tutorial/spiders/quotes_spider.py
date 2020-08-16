8ad43015cb05059b5eb2bf720b11c7d904bf847e
ZZy979 <979481894@qq.com>
2020-08-16 21:56:27 +0800

    Change QuotesSpider to support filtering by tag

--- scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/spiders/quotes_spider.py
@@ -3,20 +3,21 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/'
-    ]
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall()
             }
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
-            # yield scrapy.Request(response.urljoin(next_page))
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, self.parse)
