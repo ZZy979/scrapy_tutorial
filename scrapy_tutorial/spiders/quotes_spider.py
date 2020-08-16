986951796e04c70a6b73269933ecafe1be16c9b2
ZZy979 <979481894@qq.com>
2020-08-16 18:39:24 +0800

    Change QuotesSpider.parse to extract data and follow links; Add AuthorSpider

--- scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/spiders/quotes_spider.py
@@ -9,8 +9,14 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            # yield scrapy.Request(response.urljoin(next_page))
+            yield response.follow(next_page, callback=self.parse)
